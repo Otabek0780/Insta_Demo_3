1892d5195069b24ca99c16cb482430adadefc206
Otabek0780 <mrdunacdtm@gmail.com>
2021-01-31 19:32:11 +0900

    Initial Commit

---
+++ Insta_Demo_3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Insta_Demo_3
+//
+//  Created by kim jong moon on 31/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
